1ed2ee4bf589d06f6c7051cf1f994014cd360fd1
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-05-05 17:37:42 +0900

    Next.jsを導入

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const App = ({Component, pageProps}) => {
+  return (
+    <>
+      <Head>
+        <title>おひるーれっと</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
