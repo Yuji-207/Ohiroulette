cb9e66a7a52a6ab5c56d06c056bbc8e0f37f0922
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-05-06 18:49:24 +0900

    ルーレットを再実装

--- pages/index.js
+++ pages/index.js
@@ -18,75 +18,94 @@ import Header from '@components/header/Header';
 const Home = (props) => {
 
   const [places, setPlaces] = React.useState(props.places);
-  const [checked, setChecked] = React.useState([]);
-  const [turnning, setTurnning] = React.useState(false);
-  const [choiced, setChoiced] = React.useState('今日のお昼は？');
+  const [checked, setChecked] = React.useState(Array(places.length).fill(true));
+  const [button, setButton] = React.useState('まわす');
 
   // ルーレット
   React.useEffect(() => {
-    if (turnning) {
-
-      let index = 0;
-
+    if (button === 'とめる') {
       const timerId = setInterval(() => {
-        setChoiced(() => {
-          index = index < checked.length ? index + 1 : 0;
-          setChoiced(checked[index]);
-        })
-      }, 50);
-      
-      return () => clearInterval(timerId);  
-
+        const copied = [...places];
+        const shifted = copied.shift();
+        copied.push(shifted);
+        setPlaces(copied);
+      }, 30);
+      return () => clearInterval(timerId);
     }
-  }, [turnning]);
+  }, [button, places]);
+
+
+  const handleCheck = e => {
+    const copied = [...checked];
+    const index = String((e.target.id).slice(9));
+    copied[index] = e.target.checked;
+    setChecked(copied);
+  }
+
+  
+
+  const handleClick = () => {
+    if (button === 'まわす') {
+      setButton('とめる');
+    } else if (button === 'とめる') {
+      setButton('りせっと');
+    } else {
+      places.sort((a, b) => a.distance - b.distance);
+      setButton('まわす');
+    }
+  }
+
 
   return (
     <>
       <Header />
       <Container sx={{display: 'flex', flexFlow: 'column', justifyContent: 'space-between', textAlign: 'center', margin: '5rem 0 8rem 0'}}>
+ 
         <Typography variant="h5" component="h2" m={2}>
-          候補のお店を選択
+          {button === 'まわす' ? '候補のお店をえらぶ' : button === 'とめる' ? '今日のお昼は？' : 'ここだよ！'}
         </Typography>
         {places.map((place, i) => (
-          <Box key={i} m={2}>
-            <Card sx={{maxWidth: 345}}>
-              <CardHeader
-                action={
-                  <Checkbox defaultChecked />
-                }
-                title={
-                  <Typography variant="subtitle1" component="h3" align="left" color="text.secondary">
-                    {place.name}
+          !(button === 'りせっと' && i > 0) && (
+            <Box key={i} m={2}>
+              <Card sx={{maxWidth: 345}}>
+                <CardHeader
+                  action={
+                    <Checkbox id={'checkbox-' + i} defaultChecked onChange={handleCheck} />
+                  }
+                  title={
+                    <Typography variant="subtitle1" component="h3" align="left" color="text.secondary">
+                      {place.name}
+                    </Typography>
+                  }
+                  subheader={
+                    <Typography variant="subtitle2" component="p" align="left"color="text.secondary">
+                      {'★ ' + place.rating + ' ー ここから ' + place.distance + ' m'}
+                    </Typography>
+                  }
+                />
+                {/* <CardMedia
+                  component="img"
+                  height="140"
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  alt={place.name}
+                /> */}
+                <CardContent>
+                  <Typography variant="caption" align="left" color="text.secondary">
+                    {place.vicinity}
                   </Typography>
-                }
-                subheader={
-                  <Typography variant="subtitle2" component="p" align="left"color="text.secondary">
-                    {'★ ' + place.rating + ' ー ここから ' + place.distance + ' m'}
-                  </Typography>
-                }
-              />
-              {/* <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt={place.name}
-              /> */}
-              <CardContent>
-                <Typography variant="caption" align="left" color="text.secondary">
-                  {place.vicinity}
-                </Typography>
-              </CardContent>
-              {/* <CardActions>
-                <Button size="small">詳しく見る</Button>
-              </CardActions> */}
-            </Card>
-          </Box>
+                </CardContent>
+                {/* <CardActions>
+                  <Button size="small">詳しく見る</Button>
+                </CardActions> */}
+              </Card>
+            </Box>
+          )
         ))}
       </Container>
       <Box m={3} sx={{position: 'fixed', inset: 'auto 0 0 0', display: 'flex', justifyContent: 'center'}}>
-        <Button variant="contained">
+        <Button variant="contained" onClick={handleClick}>
           <Typography variant="h4" component="p" p={2}>
-            まわす
+            {button}
           </Typography>
         </Button>
       </Box>
