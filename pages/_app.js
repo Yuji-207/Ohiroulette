df3c9aa77053e2f158e20759e4e8110c9fa655b0
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2022-06-05 13:13:50 +0900

    サイトの説明文を変更

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,8 @@ const App = ({Component, pageProps}) => {
     <>
       <Head>
         <title>おひるーれっと</title>
-        {/* <meta property="og:url" content=" ページの URL" /> */}
-        {/* <meta property="og:type" content=" ページの種類" /> */}
-        <meta property="og:title" content=" ページの タイトル" />
+        <description>今日のお昼ごはんを決めよう！</description>
         <meta property="og:description" content=" ページの説明文" />
-        <meta property="og:site_name" content="サイト名" />
-        {/* <meta property="og:image" content=" サムネイル画像の URL" /> */}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
